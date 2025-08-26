b54e0b43e4c78736b88562c1f613d4cd49a5e1d7
AdityaPandeyCN <adityapand3y666@gmail.com>
2025-08-26 16:51:45 +0530

    making script google benchmarking based
    Signed-off-by: AdityaPandeyCN <adityapand3y666@gmail.com>

--- test/generate_test_sam.cxx
+++ test/generate_test_sam.cxx
@@ -1,15 +1,11 @@
+#include <benchmark/benchmark.h>
 #include <fstream>
 #include <random>
 #include <vector>
 #include <string>
-#include <utility>
 
-int main(int argc, char* argv[]) {
-    std::string filename = argc > 1 ? argv[1] : "samexample.sam";
-    int num_reads = argc > 2 ? std::stoi(argv[2]) : 100;
-    
+static void GenerateSAMFile(const std::string& filename, int num_reads) {
     std::mt19937 rng(42);
-    
     std::vector<std::pair<std::string, int>> chromosomes = {
         {"chr1", 249250621}, {"chr2", 243199373}, {"chr3", 198022430},
         {"chr4", 191154276}, {"chr5", 180915260}, {"chr6", 171115067},
@@ -22,6 +18,13 @@ int main(int argc, char* argv[]) {
     };
     
     const char bases[4] = {'A', 'C', 'G', 'T'};
+    
+    std::ofstream out(filename);
+    
+    for (const auto& [chrom, length] : chromosomes) {
+        out << "@SQ\tSN:" << chrom << "\tLN:" << length << "\n";
+    }
+    
     std::uniform_int_distribution<> base_dist(0, 3);
     std::uniform_int_distribution<> flag_dist(0, 1);
     std::uniform_int_distribution<> chrom_dist(0, chromosomes.size() - 1);
@@ -32,52 +35,38 @@ int main(int argc, char* argv[]) {
     std::uniform_int_distribution<> coord_dist(1, 1000);
     std::uniform_int_distribution<> mismatch_dist(0, 9);
     
-    std::ofstream out(filename);
-    
-    // Write header
-    for (const auto& [chrom, length] : chromosomes) {
-        out << "@SQ\tSN:" << chrom << "\tLN:" << length << "\n";
-    }
-    
-    // Generate reads
     for (int i = 0; i < num_reads; ++i) {
         auto [chrom, chrom_length] = chromosomes[chrom_dist(rng)];
         int read_length = 36;
         std::uniform_int_distribution<> pos_dist(1, std::max(1, chrom_length - read_length));
         int position = pos_dist(rng);
         
-        // Generate read name
         out << "SOLEXA-1GA-2_2_FC" << fc_dist(rng) 
             << ":" << lane_dist(rng) 
             << ":" << tile_dist(rng)
             << ":" << coord_dist(rng)
             << ":" << coord_dist(rng) << "\t";
         
-        // Flag, chrom, position, mapq
         out << (flag_dist(rng) * 16) << "\t"
             << chrom << "\t"
             << position << "\t"
             << 25 << "\t";
         
-        // CIGAR
         out << read_length << "M\t";
         
-        // Mate info
         out << "*\t0\t0\t";
         
-        // Sequence
         std::string sequence;
+        sequence.reserve(read_length);
         for (int j = 0; j < read_length; ++j) {
             sequence += bases[base_dist(rng)];
         }
         out << sequence << "\t";
         
-        // Quality
         for (int j = 0; j < read_length; ++j) {
             out << static_cast<char>(qual_dist(rng));
         }
         
-        // Optional fields
         int nm = mismatch_dist(rng) < 7 ? 0 : 1;
         out << "\tNM:i:" << nm;
         out << "\tX" << nm << ":i:1";
@@ -87,6 +76,27 @@ int main(int argc, char* argv[]) {
     }
     
     out.close();
+}
+
+static void BM_GenerateSAM(benchmark::State& state) {
+    for (auto _ : state) {
+        GenerateSAMFile("benchmark_temp.sam", state.range(0));
+        std::remove("benchmark_temp.sam");
+    }
+}
+
+BENCHMARK(BM_GenerateSAM)->Arg(100);
+
+int main(int argc, char** argv) {
+    if (argc >= 2) {
+        std::string filename = argv[1];
+        int num_reads = argc > 2 ? std::stoi(argv[2]) : 100;
+        GenerateSAMFile(filename, num_reads);
+        return 0;
+    }
+    
+    ::benchmark::Initialize(&argc, argv);
+    ::benchmark::RunSpecifiedBenchmarks();
     return 0;
 }
 
