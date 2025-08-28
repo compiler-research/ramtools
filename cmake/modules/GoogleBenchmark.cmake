b8a76e9010495db0f9b514973c47d8c65ccab1fa
AdityaPandeyCN <adityapand3y666@gmail.com>
2025-08-28 20:35:25 +0530

    resolving errors
    Signed-off-by: AdityaPandeyCN <adityapand3y666@gmail.com>

--- cmake/modules/GoogleBenchmark.cmake
+++ cmake/modules/GoogleBenchmark.cmake
@@ -1,35 +1,28 @@
-include(FetchContent)
+include(ExternalProject)
 
-# Try to find benchmark package first
-find_package(benchmark QUIET)
+set(GBENCHMARK_PREFIX "${CMAKE_BINARY_DIR}/googlebenchmark-prefix")
 
-if(NOT benchmark_FOUND)
-    message(STATUS "Google Benchmark not found, downloading and building with FetchContent")
-    
-    # Set options for Google Benchmark
-    set(BENCHMARK_ENABLE_TESTING OFF CACHE BOOL "Disable benchmark testing" FORCE)
-    set(BENCHMARK_ENABLE_GTEST_TESTS OFF CACHE BOOL "Disable gtest tests" FORCE)
-    set(BENCHMARK_DOWNLOAD_DEPENDENCIES ON CACHE BOOL "Download benchmark dependencies" FORCE)
-    
-    FetchContent_Declare(
-        googlebenchmark
-        GIT_REPOSITORY https://github.com/google/benchmark.git
-        GIT_TAG v1.8.4
-        GIT_SHALLOW TRUE
-    )
-    
-    FetchContent_MakeAvailable(googlebenchmark)
-endif()
+ExternalProject_Add(
+    googlebenchmark
+    GIT_REPOSITORY https://github.com/google/benchmark.git
+    GIT_TAG v1.8.4
+    GIT_SHALLOW 1
+    EXCLUDE_FROM_ALL 1
+    CMAKE_ARGS 
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+        -DBENCHMARK_ENABLE_TESTING=OFF
+        -DBENCHMARK_ENABLE_GTEST_TESTS=OFF
+    INSTALL_COMMAND ""
+    BUILD_BYPRODUCTS "${GBENCHMARK_PREFIX}/src/googlebenchmark-build/src/${CMAKE_STATIC_LIBRARY_PREFIX}benchmark${CMAKE_STATIC_LIBRARY_SUFFIX}"
+)
 
-# Resolve canonical target names to variables without creating aliases
-if(TARGET benchmark::benchmark)
-    set(BENCHMARK_LIB_TARGET benchmark::benchmark CACHE INTERNAL "Google Benchmark library target")
-elseif(TARGET benchmark)
-    set(BENCHMARK_LIB_TARGET benchmark CACHE INTERNAL "Google Benchmark library target")
-endif()
+ExternalProject_Get_Property(googlebenchmark source_dir binary_dir)
+file(MAKE_DIRECTORY ${source_dir}/include)
 
-# Verify benchmark target exists
-if(NOT DEFINED BENCHMARK_LIB_TARGET)
-    message(FATAL_ERROR "Google Benchmark target not found (neither benchmark::benchmark nor benchmark)")
-endif() 
+add_library(benchmark::benchmark IMPORTED STATIC GLOBAL)
+set_target_properties(benchmark::benchmark PROPERTIES
+    IMPORTED_LOCATION ${binary_dir}/src/${CMAKE_STATIC_LIBRARY_PREFIX}benchmark${CMAKE_STATIC_LIBRARY_SUFFIX}
+    INTERFACE_INCLUDE_DIRECTORIES "${source_dir}/include"
+)
+add_dependencies(benchmark::benchmark googlebenchmark)
 
