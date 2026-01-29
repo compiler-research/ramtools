1e358060c79738096ff469407b5e8a517deddae8
AdityaPandeyCN <adityapand3y666@gmail.com>
2026-01-29 09:51:44 +0530

    add bool header
    Signed-off-by: AdityaPandeyCN <adityapand3y666@gmail.com>

--- inc/ramcore/SamToTTree.h
+++ inc/ramcore/SamToTTree.h
@@ -1,6 +1,8 @@
 #ifndef RAMCORE_SAMTOTREE_H
 #define RAMCORE_SAMTOTREE_H
 
+#include <stdbool.h>
+
 void samtoram(const char *datafile, const char *treefile, bool index, bool split, bool cache, int compression_algorithm,
               unsigned int quality_policy);
 
