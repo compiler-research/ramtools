7faa52a1c142dd42f6abb2a7469626aef776c062
AdityaPandeyCN <adityapand3y666@gmail.com>
2026-01-29 19:42:55 +0530

    removed clang-format-ignore file
    Signed-off-by: AdityaPandeyCN <adityapand3y666@gmail.com>

--- inc/ttree/LinkDef.h
+++ inc/ttree/LinkDef.h
@@ -1,12 +1,14 @@
+// clang-format off
 #ifdef __CLING__
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class RAMRefs + ;
-#pragma link C++ class RAMIndex + ;
-#pragma link C++ class RAMRecord + ;
-#pragma link C++ class RAMNTupleRecord + ;
-#pragma link C++ class RAMNTupleIndex + ;
+#pragma link C++ class RAMRefs+;
+#pragma link C++ class RAMIndex+;
+#pragma link C++ class RAMRecord+;
+#pragma link C++ class RAMNTupleRecord+;
+#pragma link C++ class RAMNTupleIndex+;
 
 #endif
+// clang-format on
